2b326d9f9450243de1082a41f8a3a15891251423
Jonathan Hefner <jonathan@hefner.pro>
2023-12-02 21:50:19 -0600

    debug

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -47,6 +47,9 @@ module RailsAppHelpers
 
         File.write("Gemfile", gemfile)
 
+        puts "~" * 50
+        system("gem", "install", "sass-embedded")
+        puts "~" * 50
         run_command("bundle", "install")
         system("cat", "/opt/hostedtoolcache/Ruby/3.0.6/x64/lib/ruby/gems/3.0.0/extensions/x86_64-linux/3.0.0/sass-embedded-1.69.5/gem_make.out")
       end
